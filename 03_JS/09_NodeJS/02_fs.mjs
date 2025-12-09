78a023fb2ef257d9f5ecec88a7daceae284907cc
wanggang37 <wanggang37@xiaomi.com>
2025-12-09 10:17:42 +0800

    feat: 2025-12-09

--- 03_JS/09_NodeJS/02_fs.mjs
+++ 03_JS/09_NodeJS/02_fs.mjs
@@ -2,6 +2,7 @@ import { readFile } from 'node:fs';
 
 
 console.log('BEGIN');
+// 异步读取文件
 readFile('../frontEndLearning/resource/sample.txt', 'utf-8', function (err, data) {
     if (err) {
         console.log(err);
